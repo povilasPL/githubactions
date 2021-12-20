b4c0acb9a9d06f3f3b534890c9e4f1277e25e78d
Povilas Plauska <>
2021-12-20 22:28:08 +0200

    first commit

---
+++ src/__tests__/app.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import App from '../App';
+
+describe("App test", () => {
+  const Component: React.FC = () => <App />
+  
+  test("should render App component", () => {
+    render(<Component />)
+
+    expect(screen.getByTestId("app")).toBeInTheDocument();
+  })
+})
---
+++ webpack.config.prod.ts
@@ -0,0 +1,99 @@
+import path from "path";
+import { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const config: Configuration = {
+  mode: "production",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash].js",
+    publicPath: "",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: { loader: 'file-loader' },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/css/[name].[contenthash:8].css",
+      chunkFilename: "assets/css/[name].[contenthash:8].chunk.css"
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: "async",
+      minSize: 0,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 10,
+      maxInitialRequests: 10,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        common: {
+          minChunks: 2,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+};
+
+export default config;
