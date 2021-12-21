c050c5d0eaa355e0b1df97497834a7a3a2e8c05d
Povilas Plauska <>
2021-12-21 11:31:08 +0200

    fixed indentation

--- webpack.config.dev.ts
+++ webpack.config.dev.ts
@@ -1,89 +1,89 @@
 import path from "path";
 import { Configuration, HotModuleReplacementPlugin } from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import ESLintPlugin from "eslint-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const webpack: Configuration = {
-  mode: "development",
-  devtool: "cheap-module-source-map",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "assets/js/[name].[contenthash:8].js",
-    publicPath: "",
-  },
-  entry: "./src/index.tsx",
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
-            ],
-          },
-        },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' },
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: { loader: 'file-loader' },
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
+    mode: "development",
+    devtool: "cheap-module-source-map",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "assets/js/[name].[contenthash:8].js",
+        publicPath: "",
+    },
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                        ],
+                    },
+                },
             },
-          },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: { loader: "file-loader" },
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "fonts/",
+                        },
+                    },
+                ],
+            },
         ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-      filename: "index.html",
-      inject: true
-    }),
-    new MiniCssExtractPlugin({
-      filename: "assets/css/[name].[contenthash:8].css",
-      chunkFilename: "assets/css/[name].[contenthash:8].chunk.css"
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      async: false
-    }),
-    new ESLintPlugin({
-      extensions: ["js", "ts", "tsx"],
-    }),
-    new HotModuleReplacementPlugin(),
-  ].filter(Boolean),
-  devServer: {
-    static: path.join(__dirname, "build"),
-    historyApiFallback: true,
-    port: 3000,
-    open: true,
-    hot: true
-  },
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            filename: "index.html",
+            inject: true,
+        }),
+        new MiniCssExtractPlugin({
+            filename: "assets/css/[name].[contenthash:8].css",
+            chunkFilename: "assets/css/[name].[contenthash:8].chunk.css",
+        }),
+        new ForkTsCheckerWebpackPlugin({
+            async: false,
+        }),
+        new ESLintPlugin({
+            extensions: ["js", "ts", "tsx"],
+        }),
+        new HotModuleReplacementPlugin(),
+    ].filter(Boolean),
+    devServer: {
+        static: path.join(__dirname, "build"),
+        historyApiFallback: true,
+        port: 3000,
+        open: true,
+        hot: true,
+    },
 };
 
 export default webpack;
