90584948133b2aa9c46052402b1a78ee96810acc
Povilas Plauska <>
2021-12-21 23:43:17 +0200

    indentation fix

--- src/__tests__/app.test.tsx
+++ src/__tests__/app.test.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-import App from '../App';
+import App from "../App";
 
 describe("App test", () => {
-  const Component: React.FC = () => <App />
-  
-  test("should render App component", () => {
-    render(<Component />)
+    const Component: React.FC = () => <App />;
 
-    expect(screen.getByTestId("app")).toBeInTheDocument();
-  })
-})
+    test("should render App component", () => {
+        render(<Component />);
+
+        expect(screen.getByTestId("app")).toBeInTheDocument();
+    });
+});
